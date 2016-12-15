8d3f4f2b3e8c247f8c03e4ed9a0a5cdb9a4ee949
bridgeit <anujsachan@gmail.com>
2016-12-15 12:56:44 +0530

    add

---
+++ Replace.java
@@ -0,0 +1,9 @@
+public class Replace
+{
+	public static void main(String []args)
+	{
+	String s1="hello Anuj";
+	String replaceString=s1.replace("hello","");
+	System.out.println(replaceString);
+	}
+}
